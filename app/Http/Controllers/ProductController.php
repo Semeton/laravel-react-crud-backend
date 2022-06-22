772956a6af90eec6f17bce78e902d156a39ae408
Semeton Balogun <balogunsemeton@gmail.com>
2022-06-22 15:41:45 +0100

    Product Controller CRUD public functions created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
 class ProductController extends Controller
 {
     /**
@@ -14,7 +18,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return Product::select('id', 'title', 'description','image')->get();
     }
 
     /**
@@ -35,7 +39,26 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'image'=>'required|image',
+        ]);
+
+        try{
+            $imageName = Str::random().'.'.$request->image->getClientOriginalExtention();
+            Storage::disk('public')->putFileAs('product/image', $request->image,$imageName);
+            Product::create($request->post()+['image'=>$imageName]);
+
+            return response()->json([
+                'message'=>'Product Sreated Successfully.'
+            ]);
+        }catch(\Exception $e){
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message'=>'There is a problem creating a product'
+            ],500);
+        }
     }
 
     /**
@@ -46,7 +69,9 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return response()->json([
+            'product'=>$product
+        ]);
     }
 
     /**
@@ -69,7 +94,42 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'image'=>'nullable'
+        ]);
+
+        try{
+
+            $product->fill($request->post())->update();
+
+            if($request->hasFile('image')){
+
+                // remove old image
+                if($product->image){
+                    $exists = Storage::disk('public')->exists("product/image/{$product->image}");
+                    if($exists){
+                        Storage::disk('public')->delete("product/image/{$product->image}");
+                    }
+                }
+
+                $imageName = Str::random().'.'.$request->image->getClientOriginalExtension();
+                Storage::disk('public')->putFileAs('product/image', $request->image,$imageName);
+                $product->image = $imageName;
+                $product->save();
+            }
+
+            return response()->json([
+                'message'=>'Product Updated Successfully!!'
+            ]);
+
+        }catch(\Exception $e){
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message'=>'There was an error while updating a product!!'
+            ],500);
+        }
     }
 
     /**
@@ -80,6 +140,26 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        try {
+
+            if($product->image){
+                $exists = Storage::disk('public')->exists("product/image/{$product->image}");
+                if($exists){
+                    Storage::disk('public')->delete("product/image/{$product->image}");
+                }
+            }
+
+            $product->delete();
+
+            return response()->json([
+                'message'=>'Product Deleted Successfully!!'
+            ]);
+            
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message'=>'There was an error deleting the product!!'
+            ]);
+        }
     }
 }
